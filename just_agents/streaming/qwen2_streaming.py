c119bab8808396a8a01e43aee7ca981998bf4aef
Alex-Karmazin <karmazinalex@gmail.com>
2024-07-15 21:44:43 +0300

    Adding Qwen2 support

--- just_agents/streaming/qwen2_streaming.py
+++ just_agents/streaming/qwen2_streaming.py
@@ -5,58 +5,63 @@ from just_agents.memory import Memory
 from just_agents.streaming.abstract_streaming import AbstractStreaming, FunctionParser
 from just_agents.utils import rotate_completion
 import json
+from qwen_agent.llm import get_chat_model
+import litellm
 
 
 class Qwen2AsyncSession(AbstractStreaming):
 
-    def _process_function(self, parser: FunctionParser, available_tools: dict[str, Callable]):
-        function_args = json.loads(parser.arguments)
-        function_to_call = available_tools[parser.name]
+    def _process_function(self, name:str, arguments:str, available_tools: dict[str, Callable]):
+        function_args = json.loads(arguments)
+        function_to_call = available_tools[name]
         try:
             function_response = function_to_call(**function_args)
         except Exception as e:
             function_response = str(e)
-        message = {"role":"function", "content":function_response, "name":parser.name,
-                         "tool_call_id":parser.id}  # TODO need to track arguments , arguments=function_args
+        message = {
+            'role': 'function',
+            'name': name,
+            'content': function_response,
+        }  # TODO need to track arguments , arguments=function_args
         return message
 
     async def resp_async_generator(self, memory: Memory,
                                    options: dict,
                                    available_tools: dict[str, Callable]
                                    ) -> AsyncGenerator[str, None]:
-        response: ModelResponse = rotate_completion(messages=memory.messages, stream=True, options=options)
-        parser: Optional[FunctionParser] = None
-        tool_messages: list[dict] = []
-        parsers: list[FunctionParser] = []
-        deltas: list[str] = []
-        for i, part in enumerate(response):
-            delta: str = part["choices"][0]["delta"].get("content")  # type: ignore
-            if delta:
-                deltas.append(delta)
+        llm = get_chat_model(options)
+        functions = None
+        if available_tools:
+            functions = []
+            for fun_name in available_tools:
+                functions.append(litellm.utils.function_to_dict(available_tools[fun_name]))
+
+        responces = llm.chat(messages=memory.messages, functions=functions, stream=True)
+        prev_len = 0
+        messages = []
+        for i, msg in enumerate(responces):
+            messages = msg
+            content = msg[0]["content"]
+            if len(content) > 0:
+                delta = content[prev_len:]
+                prev_len = len(content)
                 yield f"data: {self._get_chunk(i, delta, options)}\n\n"
 
-            tool_calls = part["choices"][0]["delta"].get("tool_calls")
-            if tool_calls and (available_tools is not None):
-                if not parser:
-                    parser = FunctionParser(id = tool_calls[0].id)
-                if parser.parsed(tool_calls[0].function.name, tool_calls[0].function.arguments):
-                    tool_messages.append(self._process_function(parser, available_tools))
-                    parsers.append(parser)
-                    parser = None #maybe Optional?
+        memory.add_messages(messages)
+        function_call = messages[-1].get('function_call', None)
+        if function_call and available_tools:
+            memory.add_message(self._process_function(function_call["name"], function_call['arguments'], available_tools))
 
-        if len(tool_messages) > 0:
-            memory.add_message(self._get_tool_call_message(parsers))
-            for message in tool_messages:
-                memory.add_message(message)
-            response = rotate_completion(messages=memory.messages, stream=True, options=options)
-            deltas = []
-            for i, part in enumerate(response):
-                delta: str = part["choices"][0]["delta"].get("content")  # type: ignore
-                if delta:
-                    deltas.append(delta)
+            responces = llm.chat(messages=memory.messages, functions=functions, stream=True)
+            prev_len = 0
+            messages = []
+            for i, msg in enumerate(responces):
+                messages = msg
+                content = msg[0]["content"]
+                if len(content) > 0:
+                    delta = content[prev_len:]
+                    prev_len = len(content)
                     yield f"data: {self._get_chunk(i, delta, options)}\n\n"
-            memory.add_message({"role":"assistant", "content":"".join(deltas)})
-        elif len(deltas) > 0:
-            memory.add_message({"role":"assistant", "content":"".join(deltas)})
 
+            memory.add_messages(messages)
         yield "data: [DONE]\n\n"
