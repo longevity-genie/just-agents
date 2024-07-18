437a65a27403106f1ed35e885ca4a217e62e1dca
Alex-Karmazin <karmazinalex@gmail.com>
2024-07-18 23:59:55 +0300

    multi function calling

--- just_agents/streaming/qwen2_streaming.py
+++ just_agents/streaming/qwen2_streaming.py
@@ -11,9 +11,10 @@ import litellm
 
 class Qwen2AsyncSession(AbstractStreaming):
 
-    def _process_function(self, name:str, arguments:str, available_tools: dict[str, Callable]):
+    def _process_function(self, name: str, arguments: str, available_tools: dict[str, Callable]):
         function_args = json.loads(arguments)
         function_to_call = available_tools[name]
+        print(function_to_call, function_args)
         try:
             function_response = function_to_call(**function_args)
         except Exception as e:
@@ -36,23 +37,11 @@ class Qwen2AsyncSession(AbstractStreaming):
             for fun_name in available_tools:
                 functions.append(litellm.utils.function_to_dict(available_tools[fun_name]))
 
-        responces = llm.chat(messages=memory.messages, functions=functions, stream=True)
-        prev_len = 0
-        messages = []
-        for i, msg in enumerate(responces):
-            messages = msg
-            content = msg[0]["content"]
-            if len(content) > 0:
-                delta = content[prev_len:]
-                prev_len = len(content)
-                yield f"data: {self._get_chunk(i, delta, options)}\n\n"
-
-        memory.add_messages(messages)
-        function_call = messages[-1].get('function_call', None)
-        if function_call and available_tools:
-            memory.add_message(self._process_function(function_call["name"], function_call['arguments'], available_tools))
-
-            responces = llm.chat(messages=memory.messages, functions=functions, stream=True)
+        proceed = True
+        while proceed:
+            proceed = False
+            responces = llm.chat(messages=memory.messages, functions=functions, stream=True,
+                                 extra_generate_cfg=dict(parallel_function_calls=True))
             prev_len = 0
             messages = []
             for i, msg in enumerate(responces):
@@ -63,5 +52,12 @@ class Qwen2AsyncSession(AbstractStreaming):
                     prev_len = len(content)
                     yield f"data: {self._get_chunk(i, delta, options)}\n\n"
 
+            fncall_msgs = [rsp for rsp in messages if rsp.get('function_call', None)]
             memory.add_messages(messages)
+            # function_call = messages[-1].get('function_call', None)
+            if fncall_msgs and len(fncall_msgs) > 0 and available_tools:
+                proceed = True
+                for msg in fncall_msgs:
+                    function_call = msg['function_call']
+                    memory.add_message(self._process_function(function_call["name"], function_call['arguments'], available_tools))
         yield "data: [DONE]\n\n"
