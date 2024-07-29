52a7182d58ad62179cb3de2961cdef9cab74c9f5
Anton Kulaga <antonkulaga@pop-os.localdomain>
2024-07-29 21:05:01 +0300

    added function tests on genetics

---
+++ tests/mock.py
@@ -0,0 +1,33 @@
+from typing import List
+
+def hybrid_search(query: str) -> str:
+    """Mock function for hybrid search."""
+    return f"Mocked hybrid search result for query: {query}"
+
+def rsid_lookup(rsid: str) -> str:
+    """Mock function for rsid lookup."""
+    return f"{rsid} has pro-longevity influence"
+
+def gene_lookup(gene: str) -> str:
+    """Mock function for gene lookup."""
+    return f"Mocked information for gene: {gene}"
+
+def pathway_lookup(pathway: str) -> str:
+    """Mock function for pathway lookup."""
+    return f"Mocked information for pathway: {pathway}"
+
+def disease_lookup(disease: str) -> str:
+    """Mock function for disease lookup."""
+    return f"Mocked information for disease: {disease}"
+
+def sequencing_info() -> str:
+    """Mock function for sequencing info."""
+    return "Mocked DNA sequencing information"
+
+def _process_sql(sql_query: str) -> List[dict]:
+    """Mock function for processing SQL query."""
+    return [{"mocked_key": "mocked_value"}]
+
+def clinical_trails_full_trial(trial_id: str) -> dict:
+    """Mock function for full clinical trial details."""
+    return {"trial_id": trial_id, "mocked_detail": "This is a mocked clinical trial detail"}
--- tests/test_session.py
+++ tests/test_session.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 import just_agents.llm_options
 from just_agents.llm_session import LLMSession
 import asyncio
+from mock import *
 
 
 def get_current_weather(location: str):
@@ -49,6 +50,19 @@ def test_stream_llama_function_calling():
     assert "72" in result
     assert "22" in result
     assert "10" in result
+
+
+def test_stream_genetics_llama_function_calling():
+    load_dotenv()
+    session: LLMSession = LLMSession(
+        llm_options=just_agents.llm_options.LLAMA3_1,
+        tools=[hybrid_search, rsid_lookup, gene_lookup, pathway_lookup, disease_lookup, sequencing_info, clinical_trails_full_trial]
+    )
+    stream = session.stream("What is the influence of different alleles in rs10937739?")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(process_stream(stream))
+    result = session.memory.last_message["content"]
+    assert "pro-longevity influence"
 
 @pytest.mark.skip(reason="so far qwen inference we are using has issues with json function calling")
 def test_async_gwen2_function_calling():
