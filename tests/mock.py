44427635624a02193dbbdd570a1cf3af811cc52c
Alex-Karmazin <karmazinalex@gmail.com>
2024-08-08 17:20:15 +0300

    adding print to mock for tracking function calls

--- tests/mock.py
+++ tests/mock.py
@@ -2,33 +2,42 @@ from typing import List
 
 def hybrid_search(query: str) -> str:
     """Mock function for hybrid search."""
+    print(f"hybrid_search({query})")
     return f"Mocked hybrid search result for query: {query}"
 
 def rsid_lookup(rsid: str) -> str:
     """Retrieves information about rsid. It could be the effects of longevity or its influence on diseases.
      rsid is identifier for single nucleotide polymorphism, and looks like rs123 where instead of 123 could be any integer."""
+    print(f"rsid_lookup({rsid})")
     return f"{rsid} has pro-longevity influence"
 
 def gene_lookup(gene: str) -> str:
     """Mock function for gene lookup."""
+    print(f"gene_lookup({gene})")
     return f"Mocked information for gene: {gene}"
 
 def pathway_lookup(pathway: str) -> str:
     """Mock function for pathway lookup."""
+    print(f"pathway_lookup({pathway})")
     return f"Mocked information for pathway: {pathway}"
 
 def disease_lookup(disease: str) -> str:
     """Mock function for disease lookup."""
+    print(f"disease_lookup({disease})")
     return f"Mocked information for disease: {disease}"
 
 def sequencing_info() -> str:
     """Mock function for sequencing info."""
+    print("sequencing_info()")
     return "Mocked DNA sequencing information"
 
 def _process_sql(sql_query: str) -> List[dict]:
     """Mock function for processing SQL query."""
+    print(f"_process_sql({sql_query})")
     return [{"mocked_key": "mocked_value"}]
 
 def clinical_trails_full_trial(trial_id: str) -> dict:
     """Mock function for full clinical trial details."""
+    print(f"clinical_trails_full_trial({trial_id})")
     return {"trial_id": trial_id, "mocked_detail": "This is a mocked clinical trial detail"}
+
