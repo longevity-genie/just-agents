bd597616c8234becd3b8845c80b0c8e374ed5e66
Alex-Karmazin <karmazinalex@gmail.com>
2024-11-01 20:54:17 +0200

    Very basic reflaction implementation.

---
+++ just_agents/reflection_agent.py
@@ -0,0 +1,31 @@
+from typing import Union, Dict, Sequence, AsyncGenerator, Any
+from just_agents.interfaces.IAgent import IAgent
+
+ITERATIONS = "iterations"
+CRITIC_PROMPT = "critic_prompt"
+STOP_WORD = "stop_word"
+
+class ReflectionAgent(IAgent):
+
+    def __init__(self, agent_schema: dict = None, author: IAgent = None, critic: IAgent = None):
+        self.author: IAgent = author
+        self.critic: IAgent = critic
+        self.agent_schema: dict = agent_schema
+        if self.agent_schema is None:
+            self.agent_schema = dict()
+
+    def query(self, query_input: Union[str, Dict, Sequence[Dict]]) -> str:
+        solution = self.author.query(query_input)
+        iterations = self.agent_schema.get(ITERATIONS, 3)
+        critic_prompt = self.agent_schema.get(CRITIC_PROMPT, "Review this output and provide specific constructive criticism: ")
+        stop_word = self.agent_schema.get(STOP_WORD, "done")
+        for _ in range(iterations):
+            feedback = self.critic.query(critic_prompt + "'''"+solution+"'''")
+            if feedback.lower().strip() == stop_word:
+                break
+            solution = self.author.query(feedback)
+
+        return solution
+
+    def stream(self, query_input: Union[str, Dict, Sequence[Dict]]) -> AsyncGenerator[Any, None]:
+        pass
